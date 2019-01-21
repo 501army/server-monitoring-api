d0001f6af1a21659491c24907c6f7c3066434d67
Gavinda Kinandana <gavindakinandana@gmail.com>
2019-01-21 11:22:59 +0700

    delete useless function

--- sysinfo/sysinfo.js
+++ sysinfo/sysinfo.js
@@ -30,18 +30,6 @@ exports.monitor = (req, res) => {
     })
 }
 
-// let formatTime = (duration)=>{
-//     var seconds = parseInt((duration / 1000) % 60),
-//     minutes = parseInt((duration / (1000 * 60)) % 60),
-//     hours = parseInt((duration / (1000 * 60 * 60)) % 24)
-
-//     hours = (hours < 10) ? "0" + hours : hours
-//     minutes = (minutes < 10) ? "0" + minutes : minutes
-//     seconds = (seconds < 10) ? "0" + seconds : seconds
-
-//     return hours + ":" + minutes + ":" + seconds
-// }
-
 String.prototype.toHHMMSS = function () {
     var sec_num = parseInt(this, 10); // don't forget the second param
     var hours   = Math.floor(sec_num / 3600);
